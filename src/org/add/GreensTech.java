ebdca19402b20e9cee3b4a7b08ab710a983c830c
vssanthosh772 <vssanthosh7722gmail.com>
2023-07-07 21:27:04 +0530

    java Project

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,28 @@
+package org.add;
+
+public class GreensTech {
+	private void greensVelachery() {
+		System.out.println("greensVelachery");
+	}
+
+	private void greensAdayar() {
+		System.out.println("greensAdayar");
+	}
+
+	private void greensTambaram() {
+		System.out.println("greensTambaram");
+	}
+
+	private void greensaAnnaNagar() {
+		System.out.println("greensAnnaNagar");
+	}
+
+	public static void main(String[] args) {
+
+		GreensTech v = new GreensTech();
+		v.greensVelachery();
+		v.greensAdayar();
+		v.greensTambaram();
+		v.greensaAnnaNagar();
+	}
+}
